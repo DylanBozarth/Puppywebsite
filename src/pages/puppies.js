e203c930287ca2aa7fd0e0054496a15e055ea5e0
Dylan <dylanabozarth@gmail.com>
2020-09-10 10:03:44 -0700

    puppy carosuel

--- src/pages/puppies.js
+++ src/pages/puppies.js
@@ -1,31 +1,39 @@
 import React from "react";
-
+import Slider from "react-slick";
 import useTitle from "../hooks/useTitle";
-import { Card, Container } from "react-bootstrap";
 
+import { Card, Container } from "react-bootstrap";
 import _ from "lodash";
 
 function Puppies({ propdata }) {
   const puppies = _.get(propdata, "puppies", []);
   useTitle("Your Next Dog");
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  };
   return (
-    <div className="puppypage">
-    
-      
+    <div className="wrapper">
+    <h2>Puppies</h2>
+       <Slider {...settings}>
       {puppies.map((puppies) => (
-        <div className="box">
-        <div id="card-container">
-      <div id="card">
-        <div className="front face">
-          
-          <img className="cardimage" src={puppies.image} alt={puppies.name} />
-          <h1>{puppies.name}</h1>
-          <p>{puppies.price}</p>
+       <div>
+        
+        
+        <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={puppies.image} alt={puppies.name} />
+            <Card.Body>
+              <Card.Title className="black">{puppies.name}</Card.Title>
+              <Card.Text className="black">{puppies.description}</Card.Text>
+            </Card.Body>
+          </Card>
+         
         </div>
-        <div className="back face">
-        <h1>{puppies.description}</h1>
-          </div></div></div></div>
-      ))}
+      ))}</Slider>
+      
       
         
   
