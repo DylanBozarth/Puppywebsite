432e08504c6bfd9b28d906197e2fee1b0e723c39
Dylan <dylanabozarth@gmail.com>
2020-09-11 10:51:57 -0700

    now good cards

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Card, Container, Row, Col, Jumbotron, Image} from "react-bootstrap";
+import { Card, Container, Row, Col, Jumbotron, Image } from "react-bootstrap";
 import useTitle from "../hooks/useTitle";
 import _ from "lodash";
 
@@ -33,71 +33,106 @@ function Home({ propdata }) {
           style={{ backgroundImage: "url(../puppies/labs.jpg)" }}
         ></div>
       </div>
-      
-      <div className="puppysegment"><div className="pageline"></div>
+      <div className="puppysegment">
+        <div className="pageline"></div>
         <h1 className="puppytitle">Your next dog</h1>
-        
- 
 
-    <Container className="homepuppies">
-    <Row>
-    { goldendoodles.slice(0, 1).map((goldendoodles) => (
-        <Col>
-        <Card style={{ width: '20rem' }}>
-       
-        <Card.Img height="300px" width="250px" className=""fluid={true} variant="top" src={goldendoodles.image} alt={goldendoodles.name}  rounded />
-        
-        <Card.Body>
-          <Card.Title className="black">{goldendoodles.name}</Card.Title>
-          <Card.Text className="carddesc">
-            {goldendoodles.description}
-          </Card.Text>
-        </Card.Body>
-      </Card></Col>
-      ))}
-      { poodles.slice(0, 1).map((poodles) => (
-        <Col>
-        <Card style={{ width: '20rem' }}>
-        <Col >
-        <Card.Img height="300px" width="250px" className="" variant="top" src={poodles.image} alt={poodles.name} />
-        </Col>
-        <Card.Body>
-          <Card.Title className="black">{poodles.name}</Card.Title>
-          <Card.Text className="carddesc">
-            {poodles.description}
-          </Card.Text>
-        </Card.Body>
-      </Card></Col>
-      ))}
-      { labradoodles.slice(0, 1).map((labradoodles) => (
-        <Col>
-        <Card style={{ width: '20rem' }} className="homecard">
-        <Col >
-        <Card.Img height="300px" width="250px" className="" variant="top" src={labradoodles.image} alt={labradoodles.name} />
-        </Col>
-        <Card.Body>
-          <Card.Title className="black">{labradoodles.name}</Card.Title>
-          <div className="cardesc">
-          <Card.Text className="black">
-            {labradoodles.description}
-          </Card.Text></div>
-        </Card.Body>
-      </Card></Col>
-      ))}
-    </Row></Container>
-    <div className="seemorepuppies">See More Puppies!</div>
-    <div className="pageline"></div>
-    <div className="aboutsegment">
-      <h1>Good pups come from good Hoomans</h1>
-      
-      <img className="banner" src="./puppies/family.jpg"></img>
-      <Jumbotron>
-        <h1>wewewe</h1>
-      </Jumbotron>
-    </div>
+        <Container className="homepuppies">
+          <Row>
+            {goldendoodles.slice(0, 1).map((goldendoodles) => (
+              <Col >
+                <Card className="homecard" style={{ width: "20rem" }}>
+                  <Card.Img
+                    height="300px"
+                    width="280px"
+                   
+                    fluid={true}
+                    variant="top"
+                    src={goldendoodles.image}
+                    alt={goldendoodles.name}
+                    
+                    className="homecard"
+                  />
+
+                  <Card.Body className="homecardbot">
+                    <Card.Title className="black">
+                      {goldendoodles.name}
+                    </Card.Title>
+                    <div className="cardesc">
+                      <Card.Text className="black">
+                        {goldendoodles.description}
+                      </Card.Text>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+            {poodles.slice(0, 1).map((poodles) => (
+              <Col>
+                <Card style={{ width: "20rem" }} className="homecard">
+                  <Col>
+                    <Card.Img
+                      height="300px"
+                      width="280px"
+                      className="homecard"
+                      variant="top"
+                      src={poodles.image}
+                      alt={poodles.name}
+                      fluid={true}
+                    />
+                  </Col>
+                  <Card.Body className="homecardbot">
+                    <Card.Title className="black">{poodles.name}</Card.Title>
+                    <div className="cardesc">
+                      <Card.Text className="black">
+                        {poodles.description}
+                      </Card.Text>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+            {labradoodles.slice(0, 1).map((labradoodles) => (
+              <Col>
+                <Card style={{ width: "20rem" }} className="homecard">
+                  <Col>
+                    <Card.Img
+                      height="300px"
+                      width="280px"
+                      className="homecard"
+                      variant="top"
+                      src={labradoodles.image}
+                      alt={labradoodles.name}
+                      fluid={true}
+                    />
+                  </Col>
+                  <Card.Body className="homecardbot">
+                    <Card.Title className="black">
+                      {labradoodles.name}
+                    </Card.Title>
+                    <div className="cardesc">
+                      <Card.Text className="black">
+                        {labradoodles.description}
+                      </Card.Text>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+        <div className="seemorepuppies">See More Puppies!</div>
+        <div className="pageline"></div>
+        <div className="aboutsegment">
+          <h1>Good pups come from good Hoomans</h1>
+
+          <img className="banner" src="./puppies/family.jpg"></img>
+          <Jumbotron>
+            <h1>wewewe</h1>
+          </Jumbotron>
+        </div>
       </div>
-      ;
-      <div className="pageline"></div>
+      ;<div className="pageline"></div>
     </div>
   );
 }
