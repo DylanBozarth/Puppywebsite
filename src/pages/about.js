b9e8edfcc4c37fff1fb38a8693f48d0d2a609964
Dylan <dylanabozarth@gmail.com>
2020-09-11 15:42:57 -0700

    about page prototype

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 
+import { Container, Col, Row, Image } from "react-bootstrap";
 import useTitle from "../hooks/useTitle";
 
 function About() {
-    useTitle('Reputable Breeders')
-    return(
-<div className="page">We breed labrodoodles, they're pretty cute.</div>
-    )
+  useTitle("Reputable Breeders");
+  return (
+    <Container fluid>
+      <Row>
+        <Col>
+          <Image src="./puppies/family.jpg" fluid></Image>
+        </Col>
+        <Col>
+          <h1>About us</h1>{" "}
+          <h4>WE breed labrodoodles and they're fucking cute</h4> <br />
+          <h4>WE breed labrodoodles and they're fucking cute</h4> <br />
+          <h4>WE breed labrodoodles and they're fucking cute</h4> <br />
+          <h4>WE breed labrodoodles and they're fucking cute</h4> <br />
+          
+        </Col>
+      </Row>
+    </Container>
+  );
 }
-export default About
+export default About;
