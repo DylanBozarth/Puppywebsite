cd1ba2eba384192a53ce69bdb243748b630df961
Dylan <dylanabozarth@gmail.com>
2020-09-14 15:09:07 -0700

    working checkboxes

--- src/pages/application.js
+++ src/pages/application.js
@@ -35,8 +35,31 @@ setDogSize = () => {
         <label className="formtitle">Phone:</label>
         <input type="text" name="Phone" />
         <label className="formtitle">Interested in:</label>
-        <Badge color="primary" pill className="Badge">Big Dogs</Badge> <Badge color="primary" className="Badge" pill>Small Dogs</Badge> <Badge className="Badge" color="primary" pill>No preference</Badge>
-        <input type="text" name="Dog size" value={this.state.dogSize} />
+        <div className="checkboxes">
+          <h3>What are you interested in? Select all that apply.</h3>
+          <div className="leftboxes">
+          <div className="radio ">
+            <label>
+              <input type="checkbox" className="checkbox" name="Big dog" />1 :
+              Big dog
+            </label>
+            <label>
+              <input type="checkbox" className="checkbox" name="Medium dog" />1 :
+              Medium dog
+            </label>
+            <label>
+              <input type="checkbox" className="checkbox" name="Small dog" />1 :
+              Small pup
+            </label>
+            <label>
+              <input type="checkbox" className="checkbox" name="No preference" />1 :
+              No preference
+            </label>
+          </div>
+          
+         </div>
+</div>
+        <input type="text" name="Dog size"  />
         {status === "SUCCESS" ? <p>Thanks!</p> : <button>Submit</button>}
         {status === "ERROR" && <p>Ooops! There was an error.</p>}
       </form></div>
