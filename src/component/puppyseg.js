42ba93d501538a5f210c4e7bbc577a2f126455e8
Dylan <dylanabozarth@gmail.com>
2020-09-16 12:45:35 -0700

    styling woes

--- src/component/puppyseg.js
+++ src/component/puppyseg.js
@@ -4,22 +4,21 @@ import { IconContext } from "react-icons";
 import { NavLink } from "react-router-dom";
 import styled from 'styled-components';
 
-export default function PuppySeg() {
+function PuppySeg() {
     return(
     <div className="puppysegment">
         <h1 className="puppytitle">"Dogs can cure disease, if that disease is depression"</h1>
 
         <div className="row puppyrow no-gutters prva">
           <div className="col-sm-4">
-            
-            <StyledNavLink to="/about">
+            <StyledNavLink to="/about" className="nohighlight">
               <img
                 className="homecard portrait"
                 height="300px"
                 width="280px"
                 src="./puppies/family.jpg"
               ></img>
-              <h1 className="hometitle">Info</h1>
+              <h1 className="hometitle">About us</h1>
               <p className="homeinfo">
                 {" "}
                 More infoMore infoMore infoMore infoMore infoMore infoMore
@@ -32,14 +31,14 @@ export default function PuppySeg() {
           </div>
 
           <div className="col-sm-4">
-            <NavLink to="/puppies" className="nohighlight">
+            <StyledNavLink to="/puppies" className="nohighlight">
               <img
                 className="homecard portrait"
                 height="300px"
                 width="280px"
                 src="./puppies/shiba.jpg"
               ></img>
-              <h1 className="hometitle">Info</h1>
+              <h1 className="hometitle">Puppies</h1>
               <p className="homeinfo">
                 {" "}
                 More infoMore infoMore infoMore infoMore infoMore infoMore
@@ -48,7 +47,7 @@ export default function PuppySeg() {
               <IconContext.Provider
       value={{ color: 'black', size: '50px', className: "arrows"}}
     ><MdKeyboardArrowRight  /> <MdKeyboardArrowRight /> <MdKeyboardArrowRight /></IconContext.Provider>
-            </NavLink>
+            </StyledNavLink>
           </div>
           <div className="col-sm-4">
             <a
@@ -62,7 +61,7 @@ export default function PuppySeg() {
                 width="280px"
                 src="./puppies/shiba.jpg"
               ></img>
-              <h1 className="hometitle">Info</h1>
+              <h1 className="hometitle">Pawtree food</h1>
               <p className="homeinfo">
                 {" "}
                 More infoMore infoMore infoMore infoMore infoMore infoMore
@@ -81,4 +80,11 @@ const StyledNavLink = styled(NavLink)`
   text-emphasis: none;
   text-decoration: none;
   color: black;
+  &:hover {
+    text-emphasis: none;
+    text-decoration: none;
+    color: black;
+  }
 `;
+
+export default PuppySeg
