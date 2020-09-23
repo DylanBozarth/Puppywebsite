72fed026553ca3f444f798c566fb712c554f7ecd
Dylan <dylanabozarth@gmail.com>
2020-09-22 20:23:16 -0700

    added instagram link

--- src/component/instafeed.js
+++ src/component/instafeed.js
@@ -1,20 +1,25 @@
 import React from 'react';
-
-import { NavLink } from "react-router-dom";
+import {
+ 
+  NavLink
+  
+} from "reactstrap";
 
 function Instafeed() {
     return(
         <div>
 <div className="cloud">
-  <NavLink to="/application" className="nohighlight">
+  <NavLink href="/application" className="nohighlight">
           <img src="./assets/cloud.png" alt="puppycloud" className="puppycloud img-fluid"  ></img>
           
          
         </NavLink></div>
         <div className="instagrambox">
           {" "}
-          <div className="cover puppytitle">
-            <h1 className="puppytitle">Follow us on Social Media </h1>
+          <div className="cover container-fluid">
+          <NavLink href="https://www.instagram.com/jymr_doodles/" className="nohighlight">
+           <img className="img-fluid" src="./assets/instagramlogo.png"></img>
+           </NavLink>
           </div>
           <div className="elfsight-app-343de7b1-10c9-4569-80a1-2e487a1d8b07 instafeed"></div>
           
