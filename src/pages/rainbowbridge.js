b8a3418e2f225b7abc64710067778efb39dd75c1
Dylan <dylanabozarth@gmail.com>
2020-09-30 22:56:09 -0700

    SOF question

--- src/pages/rainbowbridge.js
+++ src/pages/rainbowbridge.js
@@ -1,21 +1,17 @@
 import React from "react";
 import _ from "lodash";
-import ImageGallery from 'react-image-gallery';
+import ImageGallery from "react-image-gallery";
 function RainbowBridge({ propdata }) {
   const pastdogs = _.get(propdata, "pastdogs", []);
   return (
-    <ImageGallery items={pastdogs} showFullscreenButton={false}>
-      {pastdogs.map((pastdogs) => (
-        <div className="slidertab">
-          <div>
-            <h1 className="slidertext">{pastdogs.name}</h1>
-            
-            <p>{pastdogs.description}</p>
-            <p>{pastdogs.year}</p>
-          </div>
-        </div>
-      ))}
-    </ImageGallery>
+    <div className="container-fluid slider">
+      <ImageGallery
+        className=""
+        items={pastdogs}
+        showFullscreenButton={false}
+        autoplay={true}
+      ></ImageGallery>
+    </div>
   );
 }
 export default RainbowBridge;
