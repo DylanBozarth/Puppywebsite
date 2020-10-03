14919653f425061c0fa74b24c32752e2b9ff4405
Dylan <dylanabozarth@gmail.com>
2020-10-03 11:40:42 -0700

    breed info page done

--- src/pages/breedinfo.js
+++ src/pages/breedinfo.js
@@ -8,26 +8,30 @@ function BreedInfo() {
     <div className="container-fluid breedinfopage">
       <h1 className="breedinfotitle ">Doodle facts</h1>
       <div className="row puppyrow no-gutters prva">
-      <div className="col-sm-4"><div className="breedinfo">
-          <h3>Info dump </h3>
-          <p>
+      <div className="col-lg-4">
+          <img
+            className="breedinfoimg img-thumbnail "
+            
+            src="./puppies/flower.jpg"
+            alt="poodle"
+          /><h3 className="breedinfo">Info dump </h3>
+          <p className="breedinfo">
             The Poodle is a dog breed that comes in three varieties: Standard
             Poodle, Miniature Poodle, and Toy Poodle. The origin of the breed is
             still discussed, with a prominent dispute over whether the poodle
             descends from Germany as a type of water dog, or from the French
             Barbet.[5]
-          </p> </div>
-          <img
-            className="breedinfoimg img-fluid"
-            height="300px"
-            width="280px"
-            src="./puppies/godcopilot.jpg"
-            alt="poodle"
-          />
+          </p> 
+          
         </div>
-        <div className="col-sm-4">
-         <div className="breedinfo"> <h3>Labradoodles</h3>
-          <p>
+        <div className="col-lg-4">
+        <img
+            className="breedinfoimg  img-thumbnail"
+           
+            src="./puppies/sleepybow.JPG"
+            alt="Labradoodle"
+          /><h3 className="breedinfo">Labradoodles</h3>
+          <p className="breedinfo">
             A Labradoodle (/ˈlæbrəduːdəl/) is a crossbreed dog created by
             crossing the Labrador Retriever and the Standard, Miniature, or Toy
             poodle. The term first appeared in 1955, but was not initially
@@ -36,33 +40,26 @@ function BreedInfo() {
             hypoallerginc coat as their poodle ancestors, assuming that the have
             been produced by a breeder well versed in genetic testing and
             selective breeding.
-          </p></div>
-          <img
-            className="breedinfoimg img-fluid"
-            height="300px"
-            width="280px"
-            src="./puppies/falldog.jpg"
-            alt="Labradoodle"
-          />
+          </p>
+          
         </div>
-        <div className="col-sm-4">
-        <div className="breedinfo"><h3>GoldenDoodle </h3>
-          <p>
+        <div className="col-lg-4">
+        
+          <img
+            className="breedinfoimg img-thumbnail"
+            
+            src="./puppies/smiledog.jpg"
+            alt="Goldendoodle"
+          /><h3 className="breedinfo">GoldenDoodle </h3>
+          <p className="breedinfo">
             A Goldendoodle is a dog crossbreed bred from a Golden Retriever and
             a Poodle, their name is a portmanteau of Golden Retriever and
             Labradoodle.[1][2] Whilst primarily bred as companion dogs,
             Goldendoodles have been successfully trained as therapy dogs, guide
             dogs[2] and other forms of assistance dogs.[3]
-          </p></div>
-          <div className="img-box container-fluid">
-          <img
-            className="breedinfoimg img-fluid"
-            height="300px"
-            width="280px"
-            src="./puppies/bluebow.jpg"
-            alt="Goldendoodle"
-          />
-        </div>
+          </p>
+          
+      
         
         
         </div>
