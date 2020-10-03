41d5968d59b3ba588b95e212fb9d0ccc317cacdc
Dylan <dylanabozarth@gmail.com>
2020-10-03 15:49:31 -0700

    guardians done

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -2,23 +2,23 @@ import React from "react";
 
 function FAQ() {
   return (
-    <div className="innerpage">
+    <div className=" answerpage">
       <div className="container-fluid">
         <div className="row">
           <div className="col-sm-4">
-          <h1>Question</h1>
-            <br />
-            Answer
+          <h1 className="question">Do I have to love my dog unconditionally?</h1>
+           
+          <p className="answertext">  Yes, it's required by law. </p>
           </div>
           <div className="col-sm-4">
-          <h1>Question</h1>
-            <br />
-            Answer
+          <h1 className="question">Do I have to love my dog unconditionally?</h1>
+        
+          <p className="answertext">  Yes, it's required by law. </p>
           </div>
           <div className="col-sm-4">
-            <h1>Question</h1>
-            <br />
-            Answer
+          <h1 className="question">Do I have to love my dog unconditionally?</h1>
+           
+          <p className="answertext">  Yes, it's required by law. </p>
           </div>
         </div>
       </div>
--- src/pages/guardians.js
+++ src/pages/guardians.js
@@ -3,7 +3,51 @@ import React from 'react';
 
 function Guardians () {
     return( 
-        <div className="innerpage container-fluid">wewewe</div>
+        <div className='container-fluid innerpage'>
+        <div className="hero ">
+          <h1>Meet our team</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <img src="./assets/team.jpg" />
+           <div className="bar"></div>
+        </div>
+        <div className="container">
+        <div className="row guardianrow">
+        <div className="col-sm-4" style={{ width: "20rem" }} >
+              <img
+                className="homecard frame"
+               
+                src='./assets/1.jpg'
+                alt="Aaron"
+              />
+              <h1 className="puppyname">Aaron</h1>
+
+              <p className="puppysubtitle">Aaron has over 20 years of relavent puppy experience</p>
+            </div>
+            <div className="col-sm-4" style={{ width: "20rem" }} >
+              <img
+                className="homecard frame"
+               
+                src='./assets/2.jpg'
+                alt="Aaron"
+              />
+              <h1 className="puppyname">Tom</h1>
+
+              <p className="puppysubtitle">Tom is part canine. </p>
+            </div>
+            <div className="col-sm-4" style={{ width: "20rem" }} >
+              <img
+                className="homecard frame"
+               
+                src='./assets/3.jpg'
+                alt="Aaron"
+              />
+              <h1 className="puppyname">Emily</h1>
+
+              <p className="puppysubtitle">Emily can communicate with puppies telepathically. </p>
+            </div>
+        </div></div>
+       </div>
+     
     )
 }
 export default Guardians
