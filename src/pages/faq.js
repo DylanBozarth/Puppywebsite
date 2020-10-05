00539294ebd4306d11b4111ab695b426c587d891
Dylan <dylanabozarth@gmail.com>
2020-10-05 13:19:45 -0700

    added FAQ

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,15 +1,33 @@
 import React from "react";
-import Collapsible from 'react-collapsible';
-
+import Collapsible from "react-collapsible";
 
 function FAQ() {
   return (
     <div className=" answerpage  innerpage">
       <div className="container-fluid">
-      <Collapsible trigger="Start here">
-        <p>This is the collapsible content. It can be any element or React component you like.</p>
-        <p>It can even be another Collapsible component. Check out the next section!</p>
-      </Collapsible>
+        <div className="row">
+          <div className="col-lg-4">
+            
+          </div>
+          <div className="col-lg-4">
+            <h1 className="FAQtitle">Frequently Asked Questions</h1>
+            <Collapsible  trigger="Will these dogs be cute? "  >
+              <p className="puppysubtitle">Oh man, you have no idea. The cutest dogs, let me tell you. --Michelle </p>
+            
+            </Collapsible>
+            <Collapsible trigger="Do they play fetch "  >
+              <p className="puppysubtitle">Yes. </p>
+            
+            </Collapsible>
+            <Collapsible  trigger="Will the dog love me unconditionally?  "  >
+              <p className="puppysubtitle">Yes, or your money back.</p>
+            
+            </Collapsible>
+          </div>
+          <div className="col-lg-4">
+            
+          </div>
+        </div>
       </div>
     </div>
   );
