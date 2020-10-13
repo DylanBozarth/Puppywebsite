cd65ea3414ebdea5773042546d1b8dbfa17a62eb
Dylan <dylanabozarth@gmail.com>
2020-10-13 16:08:38 -0700

    parent page prototype

--- src/pages/parents.js
+++ src/pages/parents.js
@@ -11,12 +11,12 @@ function Parents({ propdata }) {
     <div className="container-fluid innerpage" >
       <div className="puppyseperator"> <h1 className="puppytitle">Goldendoodles</h1></div>
      
-      <div className="row parentrow  ">
-          <div className="col-2"></div>
-          {parents.slice(0, 2).map((parents) => (
+      <div className="row">
+          {parents.map((parents) => (
+            <div className="col-lg-6">
             <div
-              className="parentcard  col "
-              style={{ width: "20rem" }}
+              className=" parentcard"
+              
               key={parents.name}
             >
               <img
@@ -28,58 +28,27 @@ function Parents({ propdata }) {
               <h1 className="puppyname">{parents.name}</h1>
               <p className="puppysubtitle">{parents.age}</p>
 <a className="puppysubtitle" href={parents.geneticLink}>See my genetic analysis</a>
-              <p className="cardesc">{parents.description}</p>
-            <div className="col-2"></div></div>
+              
+             
+            </div>
+<p>Puppies from {parents.name}</p>
+            <div className="gallery">
+   <img src="http://olhar-43.net/conteudo/fotosfofas/cachorrinhos45/6.jpg" />
+   <img src="http://fotos.sofotos.org/filhotes-de-cachorros/filhotes-de-cachorro-fofo.jpg" />
+   <img src="https://www.blogdocachorro.com.br/wp-content/uploads/11-filhotes-de-cachorros-fofos-010.jpg" />
+   <img src="https://cdn3.tudosobrecachorros.com.br/wp-content/uploads/cachorros-fofos-08.jpg" />
+   <img src="https://tudosobrecachorro.files.wordpress.com/2014/06/cachorros-fofos-2.jpg?w=625" />
+   <img src="http://olhar-43.net/fotinhas/cachorrinho-fofo2.jpg" />
+</div>
+
+
+
+            </div>
           ))}
-          
+          </div>
         </div>
-        <div className="puppyseperator"> <h1 className="puppytitle">Doodle parents</h1></div>
-        <div className="row parentrow  ">
-          <div className="col-2"></div>
-          {parents.slice(2, 4).map((parents) => (
-            <div
-              className="parentcard  col "
-              style={{ width: "20rem" }}
-              key={parents.name}
-            >
-              <img
-                className="homecard frame"
-               
-                src={parents.image}
-                alt={parents.name}
-              />
-              <h1 className="puppyname">{parents.name}</h1>
-              <p className="puppysubtitle">{parents.age}</p>
-              <a className="puppysubtitle" href={parents.geneticLink}>See my genetic analysis</a>
-              <p className="cardesc">{parents.description}</p>
-            <div className="col-2"></div></div>
-          ))}
-          
-        </div>
-        <div className="puppyseperator"> <h1 className="puppytitle">Poodle Parents</h1></div>
-        <div className="row parentrow  ">
-          <div className="col-2"></div>
-          {parents.slice(4, 6).map((parents) => (
-            <div
-              className="parentcard  col "
-              style={{ width: "20rem" }}
-              key={parents.name}
-            >
-              <img
-                className="homecard frame"
-                
-                src={parents.image}
-                alt={parents.name}
-              />
-              <h1 className="puppyname">{parents.name}</h1>
-              <p className="puppysubtitle">{parents.age}</p>
-              <a className="puppysubtitle" href={parents.geneticLink}>See my genetic analysis</a>
-              <p className="cardesc">{parents.description}</p>
-            <div className="col-2"></div></div>
-          ))}
-          
-        </div>
-    </div>
+        
+    
   );
 }
 export default Parents;
