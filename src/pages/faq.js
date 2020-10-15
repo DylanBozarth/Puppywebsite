e5d2086c59867de3cda8ab3442c6fac14d60acbe
Dylan <dylanabozarth@gmail.com>
2020-10-15 09:11:43 -0700

    faq styling

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -2,33 +2,23 @@ import React from "react";
 import Collapsible from "react-collapsible";
 
 import _ from "lodash";
-function FAQPAGE({propdata}) {
+function FAQPAGE({ propdata }) {
   const Faq = _.get(propdata, "Faq", []);
   return (
-    <div className=" answerpage  innerpage">
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-lg-4">
-            
-          </div>
-          <div className="col-lg-4">
-            <h1 className="FAQtitle">Frequently Asked Questions</h1>
-            {Faq.map((Faq) => (
-            <div className=""  key={Faq.answer}>
-             
-             <Collapsible  trigger={Faq.question} >
-              <p className="puppysubtitle">{Faq.answer} </p>
-            
-            </Collapsible>
+    <div className=" answerpage ">
+      <div className="container">
+         <h1 className="FAQtitle">Frequently Asked Questions</h1><div className="row">
+         
 
+          {Faq.map((Faq) => (
+            <div className="col-lg-4">
+              <div  key={Faq.answer}>
+                <Collapsible trigger={Faq.question}>
+                  <p className="answersubtitle">{Faq.answer} </p>
+                </Collapsible>
+              </div>{" "}
             </div>
           ))}
-    
-           
-          </div>
-          <div className="col-lg-4">
-            
-          </div>
         </div>
       </div>
     </div>
