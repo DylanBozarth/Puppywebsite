b72df974143f420b503fd97984f609151c1bc042
Dylan <dylanabozarth@gmail.com>
2020-10-15 10:04:37 -0700

    before list

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -7,12 +7,24 @@ function FAQPAGE({ propdata }) {
   return (
     <div className=" answerpage ">
       <div className="container">
-         <h1 className="FAQtitle">Frequently Asked Questions</h1><div className="row">
-         
-
+        <h1 className="FAQtitle">Frequently Asked Questions</h1>
+        <div className="centertext2">
+          <p>
+            We are unable to control Mother Nature and the timing of heat cycles
+            on our girls. Due to this, estimated litters may be born 3-6 months
+            earlier or later than planned.
+          </p>
+          <p>Prices subject to change prior to placing your deposit</p>
+          <p>
+            We are open to placing our puppies in breeding homes for a nominal
+            fee. Please email us directly if you would like to buy a puppy for
+            breeding and we can discuss the options.{" "}
+          </p>
+        </div>
+        <div className="row">
           {Faq.map((Faq) => (
             <div className="col-lg-4">
-              <div  key={Faq.answer}>
+              <div key={Faq.answer}>
                 <Collapsible trigger={Faq.question}>
                   <p className="answersubtitle">{Faq.answer} </p>
                 </Collapsible>
@@ -20,6 +32,16 @@ function FAQPAGE({ propdata }) {
             </div>
           ))}
         </div>
+      </div>
+      <div className="centertext2">
+        <p>
+          JYMR Doodles occasionally keeps back puppies to continue our breeding
+          program. We reserve the right to have first pick of ALL litters born.
+        </p>
+        <p>
+          We reserve the right to deny the sell of our puppies to anyone we do
+          not feel comfortable with.
+        </p>
       </div>
     </div>
   );
