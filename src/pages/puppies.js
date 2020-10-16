cbc8c7875ec848e349065920de95fda960c18705
Dylan <dylanabozarth@gmail.com>
2020-10-15 21:14:19 -0700

    monster page

--- src/pages/puppies.js
+++ src/pages/puppies.js
@@ -5,79 +5,97 @@ import _ from "lodash";
 
 function Puppies({ propdata }) {
   const poodles = _.get(propdata, "poodles", []);
+  const poodlewaitlist = _.get(propdata, "poodlewaitlist", []);
   const labradoodles = _.get(propdata, "labradoodles", []);
+  const labradoodlewaitlist = _.get(propdata, "labradoodlewaitlist", []);
   const goldendoodles = _.get(propdata, "goldendoodles", []);
-
+  const NOMOREGOLDENDOODLES = _.get(propdata, "NOMOREGOLDENDOODLES", []);
+  const parents = _.get(propdata, "parents", []);
   useTitle("Your Next Dog");
 
   return (
-    <div className="container-fluid innerpage" >
+    <div className="container-fluid innerpage">
+      <div className="maintitle ">
+        <h1 className="centertext">Puppies</h1>
+      </div>
       <div className="puppyseperator">
-          <h1>Poodles!</h1>
-          <p>Our best sellers</p>
-        </div>
+        <h1>Poodles!</h1>
+       
+      </div> 
+      <p className="centertext">The proud parents</p>
+      <div className="row">
+        {parents.slice(0, 2).map((parents) => (
+          <div className="parentcard col-sm-6" key={parents.name}>
+            <img
+              className="homecard frame"
+              src={parents.image}
+              alt={parents.name}
+            />
+            <h1 className="puppyname">{parents.name}</h1>
+            <a className="puppysubtitle" href={parents.geneticLink}>
+              Genetic analysis
+            </a>
+            <p className="puppysubtitle">{parents.description}</p>
+          </div>
+        ))}
+      </div>
+      <div className="row puppyrow no-gutters prva">
+        {poodles.map((poodles) => (
+          <div className="col-sm-4" key={poodles.name}>
+            <img
+              className="homecard puppycard frame"
+              src={poodles.image}
+              alt={poodles.name}
+            />
+            <h1 className="puppyname">{poodles.name}</h1>
+            <p className="puppysubtitle">{poodles.price}</p>
 
-        <div className="row puppyrow no-gutters prva">
-          {poodles.map((poodles) => (
-            <div className="col-sm-4"  key={poodles.name}>
-              <img
-                className="homecard puppycard frame"
-                
-                src={poodles.image}
-                alt={poodles.name}
-              />
-              <h1 className="puppyname">{poodles.name}</h1>
-              <p className="puppysubtitle">{poodles.price}</p>
+            <p className="cardesc">{poodles.description}</p>
+          </div>
+        ))}
+      </div>
 
-              <p className="cardesc">{poodles.description}</p>
-            </div>
-          ))}
-        </div>
-      
-        <div className="puppyseperator">
-          <h1>Labradoodles</h1>
-          <p>50% poodle, 50% labrador, 100% love</p>
-        </div>
+      <div className="puppyseperator">
+        <h1>Labradoodles</h1>
+        <p>50% poodle, 50% labrador, 100% love</p>
+      </div>
 
-        <div className="row puppyrow no-gutters prva">
-          {labradoodles.map((labradoodles) => (
-            <div className="col-sm-4"  key={labradoodles.name}>
-              <img
-                className="homecard frame"
-                
-                src={labradoodles.image}
-                alt={labradoodles.name}
-              />
-              <h1 className="puppyname">{labradoodles.name}</h1>
-              <p className="puppysubtitle">{labradoodles.price}</p>
+      <div className="row puppyrow no-gutters prva">
+        {labradoodles.map((labradoodles) => (
+          <div className="col-sm-4" key={labradoodles.name}>
+            <img
+              className="homecard frame"
+              src={labradoodles.image}
+              alt={labradoodles.name}
+            />
+            <h1 className="puppyname">{labradoodles.name}</h1>
+            <p className="puppysubtitle">{labradoodles.price}</p>
 
-              <p className="cardesc">{labradoodles.description}</p>
-            </div>
-          ))}
-        </div>
-      
-        <div className="puppyseperator">
-          <h1>Goldendoodles! </h1>
-          <p>I don't know how to describe them, but they sure are cute.</p>
-        </div>
+            <p className="cardesc">{labradoodles.description}</p>
+          </div>
+        ))}
+      </div>
 
-        <div className="row puppyrow no-gutters prva">
-          {goldendoodles.map((goldendoodles) => (
-            <div className="col-sm-4"  key={goldendoodles.name}>
-              <img
-                className="homecard frame"
-               
-                src={goldendoodles.image}
-                alt={goldendoodles.name}
-              />
-              <h1 className="puppyname">{goldendoodles.name}</h1>
-              <p className="puppysubtitle">{goldendoodles.price}</p>
+      <div className="puppyseperator">
+        <h1>Goldendoodles! </h1>
+        <p>I don't know how to describe them, but they sure are cute.</p>
+      </div>
 
-              <p className="cardesc">{goldendoodles.description}</p>
-            </div>
-          ))}
-        </div>
-      
+      <div className="row puppyrow no-gutters prva">
+        {goldendoodles.map((goldendoodles) => (
+          <div className="col-sm-4" key={goldendoodles.name}>
+            <img
+              className="homecard frame"
+              src={goldendoodles.image}
+              alt={goldendoodles.name}
+            />
+            <h1 className="puppyname">{goldendoodles.name}</h1>
+            <p className="puppysubtitle">{goldendoodles.price}</p>
+
+            <p className="cardesc">{goldendoodles.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
