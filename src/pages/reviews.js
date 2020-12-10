0a95e421f864cc6cbb0368f0308b640d4e70fb40
dylan <dylanabozarth@gmail.com>
2020-12-10 09:57:08 -0800

    added reviews

--- src/pages/reviews.js
+++ src/pages/reviews.js
@@ -1,42 +1,57 @@
-import React from 'react';
+import React from "react";
 
-
-import _ from "lodash";
-
-function Reviews({propdata}) {
-    
-  const reviews = _.get(propdata, "reviews", []);
-
-  return( 
+function Reviews() {
+  return (
     <div className="container-fluid innerpage ">
-
-
-  
-{reviews
-          .filter((x) => x.image !== "")
-          .map((reviews) => (
-            <div className="row">
-            <div className="col-lg-12" key={reviews.name}>
-              
-              <div className="text-center">
-              <img
-                className="img-fluid"
-               
-                src={reviews.image}
-                alt={reviews.name}
-              />
-            </div>
-              
-              
-              
-              <h4 className="reviewname text-center">"{reviews.testimonial}"</h4>
-<p className="puppyname centertext reviewname">-{reviews.owner}</p>
-            </div>
-            </div>
-          ))} 
-          
+      <div className="row">
+        <div className="col-sm-12 text-center">
+          <img src="./puppies/rosebudreview.jpg" alt="review"></img>
+          <h3 className="reviewname">-Alexander Age 7</h3>
+          <p className="reviewname">
+            Rosie is the best dog! She is really cute and cuddly! And she likes
+            to lick a lot! I’m so grateful to have her. She is my best friend
+            and the best dog I could ever have! Love her so much!
+          </p>
         </div>
-  )
-
+        <div className="col-sm-6 text-center">
+          <img src="./puppies/buckreview.jpg" alt="review"></img>
+          <h3 className="reviewname">-Alexander Age 7</h3>
+          <p className="reviewname">
+            I first met Juan and Michelle With JYMR Doodles in the Lehi Cabelas
+            store a couple of years ago. They had the most beautiful standard
+            poodle with them. His name is Cash. He stole my heart as well as
+            their kindness and willingness to answer my questions. In speaking
+            with Michelle I found out about JYMR DOODLES! I had been talking to
+            my John about getting either a labradoodle or golden doodle. When I
+            finally retired the following spring I contacted Michelle. What a
+            great experience! They are very professional. I was impressed by her
+            knowledge and integrity. I picked my pup, we named him Buck and soon
+            went back and purchased another one so our boy would have a great
+            playmate. We named him Roo, so that would be BUCK AND ROO (I used to
+            call my twin boys my little BUCKAROOS). 
+          </p>
+        </div>
+        <div className="col-sm-6 text-center">
+          <img src="./puppies/rooreview.jpg" alt="review"></img>
+          <h3 className="reviewname">-Alexander Age 7</h3>
+          <p className="reviewname">
+           Long story short JYMR makes
+            it very easy to work with them and they have quality pups. The
+            temperament of my boys is amazing. They want to please and are good
+            listeners. I have had Queensland healers, golden retrievers, and a
+            boarder collie. The Labradoodles are by far the smartest and easiest
+            dogs I have ever had. I wanted my sister in California to have JOY
+            these pups have given me and went back to JYMR and purchased a
+            Minnie labradoodle for my sister. We both cried when we picked her
+            up. Her name is Betsy and I FaceTime with her a couple of times a
+            week with my dogs. If you are looking for a breeder that is there
+            for you and will answer questions and assist you with information, I
+            highly recommend JYMR DOODLES. My pups are very healthy and
+            beautiful.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default Reviews
+export default Reviews;
