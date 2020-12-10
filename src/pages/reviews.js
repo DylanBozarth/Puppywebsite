c8d922287bd0e1642ebd6149d009ce31ebc2a585
dylan <dylanabozarth@gmail.com>
2020-12-10 10:24:45 -0800

    fixed my mistakes

--- src/pages/reviews.js
+++ src/pages/reviews.js
@@ -1,42 +1,67 @@
-import React from 'react';
+import React from "react";
 
-
-import _ from "lodash";
-
-function Reviews({propdata}) {
-    
-  const reviews = _.get(propdata, "reviews", []);
-
-  return( 
+function Reviews() {
+  return (
     <div className="container-fluid innerpage ">
-
-
-  
-{reviews
-          .filter((x) => x.image !== "")
-          .map((reviews) => (
-            <div className="row">
-            <div className="col-lg-12" key={reviews.name}>
-              
-              <div className="text-center">
-              <img
-                className="img-fluid"
-               
-                src={reviews.image}
-                alt={reviews.name}
-              />
-            </div>
-              
-              
-              
-              <h4 className="breedinfo text-center">"{reviews.testimonial}"</h4>
-<p className="puppyname centertext reviewname">-{reviews.owner}</p>
-            </div>
-            </div>
-          ))} 
-          
+      <div className="row">
+        <div className="col-sm-12 text-center">
+          <img src="./puppies/rosebudreview.jpg" className="img-fluid"  alt="review"></img>
+          <h3 className="reviewname">-Alexander Age 7</h3>
+          <p className="reviewname">
+            Rosie is the best dog! She is really cute and cuddly! And she likes
+            to lick a lot! I’m so grateful to have her. She is my best friend
+            and the best dog I could ever have! Love her so much!
+          </p>
         </div>
-  )
-
+        
+        <div className="col-sm-6 text-center">
+          <img src="./puppies/buckreview.jpg"  className="img-fluid" alt="review"></img>
+          
+          <p className="reviewname">
+            I first met Juan and Michelle With JYMR Doodles in the Lehi Cabelas
+            store a couple of years ago. They had the most beautiful standard
+            poodle with them. His name is Cash. He stole my heart as well as
+            their kindness and willingness to answer my questions. In speaking
+            with Michelle I found out about JYMR DOODLES! I had been talking to
+            my John about getting either a labradoodle or golden doodle. When I
+            finally retired the following spring I contacted Michelle. What a
+            great experience! They are very professional. I was impressed by her
+            knowledge and integrity. I picked my pup, we named him Buck and soon
+            went back and purchased another one so our boy would have a great
+            playmate. We named him Roo, so that would be BUCK AND ROO (I used to
+            call my twin boys my little BUCKAROOS). 
+          </p>
+        </div>
+        <div className="col-sm-6 text-center">
+          <img src="./puppies/rooreview.jpg" className="img-fluid"  alt="review"></img>
+          
+          <p className="reviewname">
+           Long story short JYMR makes
+            it very easy to work with them and they have quality pups. The
+            temperament of my boys is amazing. They want to please and are good
+            listeners. I have had Queensland healers, golden retrievers, and a
+            boarder collie. The Labradoodles are by far the smartest and easiest
+            dogs I have ever had. I wanted my sister in California to have JOY
+            these pups have given me and went back to JYMR and purchased a
+            Minnie labradoodle for my sister. We both cried when we picked her
+            up. Her name is Betsy and I FaceTime with her a couple of times a
+            week with my dogs. If you are looking for a breeder that is there
+            for you and will answer questions and assist you with information, I
+            highly recommend JYMR DOODLES. My pups are very healthy and
+            beautiful.
+          </p>
+        </div>
+        
+      </div><h3 className="reviewname text-center">-John and Terry Zimmerman</h3>
+      <div className="col-lg-12 text-center">
+          <img src="./puppies/emilyreview.jpg" className="img-fluid" alt="review"></img>
+          
+          <p className="reviewname">
+           "We bought our F1B mini goldendoodle, Mila (formerly Ash), from JYMR Doodles. Mila was from the Bella and Leo litter, “The A Team.” Michelle and her team spend a lot of time socializing their puppies, and you can definitely tell. Our puppy, Mila, has a happy, outgoing, confident temperament. She has been easy and willing to train and groom. Mila stays calm during all types of sounds, like thunderstorms, music, construction, people knocking at the door, or neighborhood dogs barking. We’ve even had to chainsaw some trees in our yard while she was nearby, and it didn’t bother her a bit! She thinks everyone she meets, animals included, are her best friends. She already had great crate skills and knew a couple of basic commands when we picked her up at 8 weeks old. She slept through the night in her crate from the first week we got her, and she was mostly potty trained by 10 weeks, and now at 17 weeks, she uses a bell to ask to go out to go potty. Mila loves being held, petted, walked, and being around all people. We get compliments about her temperament and cooperation from our vet, groomer, and the entire neighborhood. Mila has also been great with riding in the car from day one. She’s been checked regularly by her vet, who has told us repeatedly that “Mila is in excellent health, and is such a happy puppy.” Our neighbor adopted a puppy that was the same age, at the same time from a DIFFERENT breeder who kept their puppies outside in a kennel, and you can definitely tell the difference from Mila, who was socialized from birth! Our neighbor has had to work extra hard on introducing crate skills and potty training for the first time, and managing their puppy’s anxiety over unusual or loud noises. Our experience with Mila has shown us the importance of early and proper socialization for puppies. Mila is a dream puppy. We researched breeders and waited to get a puppy for a long time until we felt that we had found the right fit, and we are glad we found JYMR DOODLES. We would DEFINITELY recommend JYMR Doodles to anyone looking for a happy, healthy puppy."
+          </p>
+          <h3 className="reviewname text-center">-Emily and Jeff Utah</h3>
+        </div>
+    </div>
+  );
 }
-export default Reviews
+export default Reviews;
