a7da51bf97cc2332ee3da45f2f2a37133a11229d
dylan <dylanabozarth@gmail.com>
2021-01-19 20:35:47 -0800

    almost ginal

--- src/pages/puppies.js
+++ src/pages/puppies.js
@@ -1,20 +1,25 @@
 import React from "react";
 import useTitle from "../hooks/useTitle";
 
-
 function Puppies({ propdata }) {
- 
   useTitle("Your Next Dog");
 
   return (
     <div className="container-fluid innerpage">
-     <h1 className="puppyname text-center">Upcoming litters</h1>
-     <div className="col-sm-4">
-     <img src="./parents/leoandbella.jpg" alt="parents" className="img-fluid"></img></div>
-     <div className="col-sm-8">
-       <h3 className="puppyname text-center">Waitlist</h3>
-       <li></li> <li></li> <li></li> <li></li> <li></li> <li></li>
-     </div>
+      <h1 className="puppyname text-center">Upcoming litters</h1>
+      <div className="row">
+        <div className="col-sm-4">
+          <img
+            src="./parents/leoandbella.jpg"
+            alt="parents"
+            className="img-fluid"
+          ></img>
+        </div>
+        <div className="col-sm-8">
+          <h3 className="puppyname text-center">Waitlist</h3>
+          <li className="puppyname bigtext">Test name</li> <li className="puppyname bigtext">Test name</li> <li className="puppyname bigtext">Test name</li> <li className="puppyname bigtext">Test name</li> <li className="puppyname bigtext">Test name</li>
+        </div>
+      </div>
     </div>
   );
 }
