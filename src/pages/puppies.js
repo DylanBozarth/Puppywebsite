bb330c81ec3fe7ba086266f5f37dd941a1f6c301
dylan <dylanabozarth@gmail.com>
2021-01-24 20:51:19 -0800

    images test

--- src/pages/puppies.js
+++ src/pages/puppies.js
@@ -3,27 +3,36 @@ import useTitle from "../hooks/useTitle";
 import _ from "lodash";
 
 function Puppies() {
-  
   useTitle("Your Next Dog");
 
   return (
     <div className="container-fluid innerpage">
       <div className="row">
-      <div className="col-sm-8">
-        <img src="./parents/leoandbella.jpg" className="img-fluid" alt="parents"></img>
-        <img src="./parents/glittlepreview.jpg" className="img-fluid" alt="puppies"></img>
+        <div className="col-sm-4">
+          <img
+            src="./parents/leoandbella.jpg"
+            className="img-fluid"
+            alt="parents"
+          ></img>
+          <img
+            src="./parents/glitterpreview.jpg"
+            className="img-fluid"
+            alt="puppies"
+          ></img>
+        </div>
+        <div className="col-sm-4">
+          <h1 className="text-center homeinfo ">Reserve list </h1>
+          <div className="waitlist">
+            <li>Available</li>
+            <li>Available</li>
+            <li>Available</li>
+            <li>Available</li>
+            <li>Available</li>
+            <li>Available</li>
+            <li>Available</li>
+          </div>
+        </div>
       </div>
-      <div className="col-sm-4">
-<h1 className="text-center homeinfo ">Reserve list </h1>
-<div className=""></div>
-<li>Available</li>
-<li>Available</li>
-<li>Available</li>
-<li>Available</li>
-<li>Available</li>
-<li>Available</li>
-<li>Available</li>
-     </div></div>
     </div>
   );
 }
