b14dab2c82f0c17b04d133a2563bc7884e3e1db6
dylan <dylanabozarth@gmail.com>
2021-04-23 09:41:57 -0700

    base pups layout

--- src/newpages/pups.js
+++ src/newpages/pups.js
@@ -1,20 +1,83 @@
 import React, {useState} from 'react';
-
+import useTitle from "../hooks/useTitle";
 export const NewPups = () => {
-    const [Pups, setPups] = useState();
-    Pups = [
+    useTitle("Your Next Dog");
+     
+    const [heartattack, setHeartAttack] = useState([
         {
-            name: "",
-            image: ""
-        },
-        {
-            name: "",
-            image: ""
-        }
-    ]
+        name: "Honey",
+            description: "",
+            image: "./currentpuppies/heartattack/honey.jpg"
+          },
+          {
+            name: "Hanky",
+            description: "",
+            image: "./currentpuppies/heartattack/hanky.jpg"
+          },
+          {
+            name: "Harmony",
+            description: "",
+            image: "./currentpuppies/heartattack/harmony.jpg"
+          },
+          {
+            name: "Heart",
+            description: "",
+        image: "./currentpuppies/heartattack/heart.jpg"
+          },
+          {
+            name: "Hero",
+            description: "",
+            image: "./currentpuppies/heartattack/hero.jpg"
+          },
+          {
+            name: "Hershey",
+            description: "",
+            image: "./currentpuppies/heartattack/hershey.jpg"
+          },
+          
+          {
+            name: "Hugs",
+            description: "",
+            image: "./currentpuppies/heartattack/hugs.jpg"
+          },
+          {
+            name: "Hermosa",
+            description: "Reserved",
+            image: "./currentpuppies/heartattack/hermosa.jpg"
+          },
+          {
+            name: "Hunk",
+            description: "",
+            image: "./currentpuppies/heartattack/hunk.jpg"
+          }
+    ])
+   
     return(
-        <div>
-aaa
+        <div className="innerpage container-fluid">
+            <div className="puppyname puppytitle text-center">Lily and Asher's <br /> Heart attack doods!</div>
+<div className="row">
+    
+    {heartattack
+        
+        .map((heartattack) => (
+          <div className="col-sm-4" key={heartattack.name}>
+            <img
+              className="homecard frame img-fluid"
+              src={heartattack.image}
+              alt="goldendoodle"
+            />
+            <h1 className="puppyname">{heartattack.name}</h1>
+
+            <p className="puppysubtitle">{heartattack.description}</p>
+          </div>
+        ))}
+   
+     
+     
+      
+    </div>
+    <div className="theme text-center"><img alt="labradoodles" src="./featuredPups/bernedoodles/chloeandgus.jpg" className="img-fluid homecard frame"></img></div> 
+    <h3 className="text-center puppyname">Puppies are unnamed as of 4/23</h3>
         </div>
     )
 }
