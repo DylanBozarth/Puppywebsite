b7e387cfef934e54ab251ecf00c70146e8a51695
dylan <dylanabozarth@gmail.com>
2021-04-24 21:59:23 -0700

    smaller photos

--- src/newpages/featuredpups.js
+++ src/newpages/featuredpups.js
@@ -4,15 +4,16 @@ import Carousel from 'react-elastic-carousel'
 export const Featured = () => {
     return(
         <div className="innerpage container-fluid ">
+             <div className="theme text-center"><img alt="labradoodles" src="./featuredPups/bernedoodles/chloeandgus.jpg" className="img-fluid homecard frame"></img></div> 
             <Carousel itemsToShow={1}>
- <div><img src="./featuredPups/bernedoodles/boys1.jpg" className="homecard frame img-fluid" alt="dog"></img> <p className="text-center puppyname puppytitle">Boy</p></div>
- <div><img src="./featuredPups/bernedoodles/boys2.jpg" className="homecard frame img-fluid" alt="dog"></img><p className="text-center puppyname puppytitle">Boy</p></div>
- <div><img src="./featuredPups/bernedoodles/boys3.jpg" className="homecard frame img-fluid" alt="dog"></img><p className="text-center puppyname puppytitle">Boy</p></div>
- <div><img src="./featuredPups/bernedoodles/girls1.jpg" className="homecard frame img-fluid" alt="dog"></img><p className="text-center puppyname puppytitle">Girl</p></div>
- <div><img src="./featuredPups/bernedoodles/girls2.jpg" className="homecard frame img-fluid" alt="dog"></img><p className="text-center puppyname puppytitle">Girl</p></div>
- <div><img src="./featuredPups/bernedoodles/girls3.jpg" className="homecard frame img-fluid" alt="dog"></img><p className="text-center puppyname puppytitle">Girl</p></div>
- <div><img src="./featuredPups/bernedoodles/girls4.jpg" className="homecard frame img-fluid" alt="dog"></img><p className="text-center puppyname puppytitle">Girl</p></div>
- <div><img src="./featuredPups/bernedoodles/girls5.jpg" className="homecard frame img-fluid" alt="dog"></img><p className="text-center puppyname puppytitle">Girl</p></div>
+ <div><img src="./featuredPups/bernedoodles/boys1.jpg" className="  img-fluid" alt="dog"></img> <p className="text-center puppyname puppytitle">Boy</p></div>
+ <div><img src="./featuredPups/bernedoodles/boys2.jpg" className=" img-fluid" alt="dog"></img><p className="text-center puppyname puppytitle">Boy</p></div>
+ <div><img src="./featuredPups/bernedoodles/boys3.jpg" className=" img-fluid" alt="dog"></img><p className="text-center puppyname puppytitle">Boy</p></div>
+ <div><img src="./featuredPups/bernedoodles/girls1.jpg" className=" img-fluid" alt="dog"></img><p className="text-center puppyname puppytitle">Girl</p></div>
+ <div><img src="./featuredPups/bernedoodles/girls2.jpg" className=" img-fluid" alt="dog"></img><p className="text-center puppyname puppytitle">Girl</p></div>
+ <div><img src="./featuredPups/bernedoodles/girls3.jpg" className=" img-fluid" alt="dog"></img><p className="text-center puppyname puppytitle">Girl</p></div>
+ <div><img src="./featuredPups/bernedoodles/girls4.jpg" className=" img-fluid" alt="dog"></img><p className="text-center puppyname puppytitle">Girl</p></div>
+ <div><img src="./featuredPups/bernedoodles/girls5.jpg" className="img-fluid" alt="dog"></img><p className="text-center puppyname puppytitle">Girl</p></div>
 </Carousel>
         </div>
     )
