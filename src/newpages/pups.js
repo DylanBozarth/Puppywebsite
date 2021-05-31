23b09c22ce8beef2a1189443149a81f40050f382
dylan <dylanabozarth@gmail.com>
2021-05-30 21:34:30 -0700

    first draft

--- src/newpages/pups.js
+++ src/newpages/pups.js
@@ -70,7 +70,7 @@ export const NewPups = () => {
            </Carousel>
            <h4 className="text-center puppyname">Harmony from the Heart Attack doods!</h4>
            <br />
-           <h3 className="text-center puppyname">Featured litter</h3>
+           
     <div className="theme text-center"><img alt="labradoodles" src="./featuredPups/bernedoodles/chloeandgus.jpg" className="img-fluid homecard frame"></img></div> 
     {/*<h3 className="text-center puppyname">Puppies are unnamed as of 4/23</h3> */}
     
@@ -83,7 +83,7 @@ export const NewPups = () => {
              <img src="./currentpuppies/jawDropper/jade3.jpg" className="homecard frame img-fluid" alt="puppy"></img>
             
            </Carousel> 
-           <h4  >Jade <br />
+           <h4  className="puppyname">Jade <br />
            Available</h4></div>
            <div className="col-sm-4">
 <Carousel itemsToShow={1}>
@@ -92,7 +92,7 @@ export const NewPups = () => {
              <img src="./currentpuppies/jawDropper/jaxx3.jpg" className="homecard frame img-fluid" alt="puppy"></img>
             
            </Carousel> 
-           <h4>Jaxx <br />
+           <h4  className="puppyname">Jaxx <br />
            Available</h4></div>
            <div className="col-sm-4">
 <Carousel itemsToShow={1}>
@@ -101,7 +101,7 @@ export const NewPups = () => {
              <img src="./currentpuppies/jawDropper/jena3.jpg" className="homecard frame img-fluid" alt="puppy"></img>
             
            </Carousel> 
-           <h4>Jena <br />
+           <h4  className="puppyname">Jena <br />
            Reserved</h4></div>
            <div className="col-sm-4">
 <Carousel itemsToShow={1}>
@@ -110,7 +110,7 @@ export const NewPups = () => {
              <img src="./currentpuppies/jawDropper/jara3.jpg" className="homecard frame img-fluid" alt="puppy"></img>
             
            </Carousel> 
-           <h4>Jara <br />
+           <h4  className="puppyname">Jara <br />
            Reserved</h4></div>
            
            <div className="col-sm-4">
@@ -120,7 +120,7 @@ export const NewPups = () => {
              <img src="./currentpuppies/jawDropper/jose3.jpg" className="homecard frame img-fluid" alt="puppy"></img>
             
            </Carousel> 
-           <h4>Jose <br />
+           <h4  className="puppyname">Jose <br />
            Available</h4></div>
            <div className="col-sm-4">
 <Carousel itemsToShow={1}>
@@ -129,7 +129,7 @@ export const NewPups = () => {
              <img src="./currentpuppies/jawDropper/juan3.jpg" className="homecard frame img-fluid" alt="puppy"></img>
             
            </Carousel> 
-           <h4>Juan <br />
+           <h4  className="puppyname">Juan <br />
            Available</h4></div>
            <div className="col-sm-4">
 <Carousel itemsToShow={1}>
@@ -138,7 +138,7 @@ export const NewPups = () => {
              <img src="./currentpuppies/jawDropper/judd3.jpg" className="homecard frame img-fluid" alt="puppy"></img>
             
            </Carousel> 
-           <h4>Judd <br />
+           <h4  className="puppyname">Judd <br />
            Available</h4></div>
            <div className="col-sm-4">
 <Carousel itemsToShow={1}>
@@ -147,7 +147,7 @@ export const NewPups = () => {
              <img src="./currentpuppies/jawDropper/juli3.jpg" className="homecard frame img-fluid" alt="puppy"></img>
             
            </Carousel> 
-           <h4>Juli <br />
+           <h4  className="puppyname">Juli <br />
           Available</h4></div>
           <div className="col-sm-4">
 <Carousel itemsToShow={1}>
@@ -156,8 +156,46 @@ export const NewPups = () => {
              <img src="./currentpuppies/jawDropper/joni3.jpg" className="homecard frame img-fluid" alt="puppy"></img>
             
            </Carousel> 
-           <h4>Joni <br />
+           <h4  className="puppyname">Joni <br />
            Reserved</h4></div>
+    </div>
+    <br />
+    <br />
+    <h3 className="puppyname text-center">Irresistable doods! <br /> 
+    F1b Moyan Goldendoodles</h3>
+    <div className="row">
+    <div className="col-sm-4">
+<Carousel itemsToShow={1}>
+             <img src="./currentpuppies/irresistable/ish.jpg" className="homecard frame img-fluid" alt="puppy"></img>
+             <img src="./currentpuppies/irresistable/ish2.jpg" className="homecard frame img-fluid" alt="puppy"></img>
+             <img src="./currentpuppies/irresistable/ish3.jpg" className="homecard frame img-fluid" alt="puppy"></img>
+           </Carousel> 
+           <h4  className="puppyname">Ish <br />
+           Available</h4></div>
+           <div className="col-sm-4">
+<Carousel itemsToShow={1}>
+             <img src="./currentpuppies/irresistable/issac1.jpg" className="homecard frame img-fluid" alt="puppy"></img>
+             <img src="./currentpuppies/irresistable/issac2.jpg" className="homecard frame img-fluid" alt="puppy"></img>
+             <img src="./currentpuppies/irresistable/issac3.jpg" className="homecard frame img-fluid" alt="puppy"></img>
+           </Carousel> 
+           <h4  className="puppyname">Issac <br />
+           Available</h4></div>
+           <div className="col-sm-4">
+<Carousel itemsToShow={1}>
+             <img src="./currentpuppies/irresistable/ivy1.jpg" className="homecard frame img-fluid" alt="puppy"></img>
+             <img src="./currentpuppies/irresistable/ivy2.jpg" className="homecard frame img-fluid" alt="puppy"></img>
+             <img src="./currentpuppies/irresistable/ivy3.jpg" className="homecard frame img-fluid" alt="puppy"></img>
+           </Carousel> 
+           <h4  className="puppyname">Ivy <br />
+           Available</h4></div>
+           <div className="col-sm-4">
+<Carousel itemsToShow={1}>
+             <img src="./currentpuppies/irresistable/izzy1.jpg" className="homecard frame img-fluid" alt="puppy"></img>
+             <img src="./currentpuppies/irresistable/izzy2.jpg" className="homecard frame img-fluid" alt="puppy"></img>
+             <img src="./currentpuppies/irresistable/izzy3.jpg" className="homecard frame img-fluid" alt="puppy"></img>
+           </Carousel> 
+           <h4  className="puppyname">Izzy <br />
+           Available</h4></div>
     </div>
     </div>
     )
