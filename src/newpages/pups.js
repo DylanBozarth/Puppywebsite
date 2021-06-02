081a19df20e03938456e4d698e80317765a1fce1
dylan <dylanabozarth@gmail.com>
2021-06-01 21:34:15 -0700

    k litter added

--- src/newpages/pups.js
+++ src/newpages/pups.js
@@ -150,6 +150,73 @@ export const NewPups = () => {
            <h4  className="puppyname">Izzy <br />
            Available</h4></div>
     </div>
+    <h3 className="text-center puppyname puppytitle">The K litter!</h3>
+
+      <h5 className="text-center puppyname puppytitle">
+        Names are to be decided{" "}
+      </h5>
+      <div className="row">
+        <div className="col-sm-2"></div>
+        <Carousel itemsToShow={1} className="col-sm-8">
+          <div>
+            <img
+              src="./currentpuppies/klitter/1.jpg"
+              className="homecard  img-fluid "
+              alt="dog"
+            ></img>{" "}
+            <p className="text-center puppyname puppytitle"></p>
+          </div>
+          <div>
+            <img
+              src="./currentpuppies/klitter/2.jpg"
+              className=" homecard  img-fluid "
+              alt="dog"
+            ></img>{" "}
+            <p className="text-center puppyname puppytitle"></p>
+          </div>
+          <div>
+            <img
+              src="./currentpuppies/klitter/3.jpg"
+              className="homecard  img-fluid  "
+              alt="dog"
+            ></img>{" "}
+            <p className="text-center puppyname puppytitle"></p>
+          </div>
+          <div>
+            <img
+              src="./currentpuppies/klitter/4.jpg"
+              className=" homecard  img-fluid "
+              alt="dog"
+            ></img>{" "}
+            <p className="text-center puppyname puppytitle"></p>
+          </div>
+          <div>
+            <img
+              src="./currentpuppies/klitter/5.jpg"
+              className="homecard  img-fluid  "
+              alt="dog"
+            ></img>{" "}
+            <p className="text-center puppyname puppytitle"></p>
+          </div>
+          <div>
+            <img
+              src="./currentpuppies/klitter/6.jpg"
+              className="homecard  img-fluid  "
+              alt="dog"
+            ></img>{" "}
+            <p className="text-center puppyname puppytitle"></p>
+          </div>
+          <div>
+            <img
+              src="./currentpuppies/klitter/7.jpg"
+              className=" homecard  img-fluid "
+              alt="dog"
+            ></img>{" "}
+            <p className="text-center puppyname puppytitle"></p>
+          </div>
+        </Carousel>
+</div>
+        <div className="col-sm-2"></div>
     <h2 className="text-center"><NavLink to="/application" className="puppyname text-center">Get me my puppy now!</NavLink></h2>
     </div>
     )
