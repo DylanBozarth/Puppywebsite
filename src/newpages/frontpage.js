1bb63b2611feda5704ea2f10f3aa88cb2345a194
dylan <dylanabozarth@gmail.com>
2021-06-29 13:22:54 -0700

    new home page 1

---
+++ src/newpages/frontpage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import useTitle from "../hooks/useTitle";
+import Kenburns from "../component/kenburns";
+import PuppySeg from "../component/puppyseg";
+import Instafeed from "../component/instafeed";
+
+export const FrontPage = () => {
+    useTitle("JYMR Doodles");
+return (
+    <div>a</div>
+)
+}
