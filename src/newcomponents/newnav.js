f21761da33dc4f553e2faa0d19233303e948acee
dylan <dylanabozarth@gmail.com>
2021-06-29 22:23:05 -0700

    new nav structure done

--- src/newcomponents/newnav.js
+++ src/newcomponents/newnav.js
@@ -4,17 +4,17 @@ export const NewNav = () => {
   return (
       <div className="container-fluid">
     <div className="  row">
-  <div className="nav-item col-sm-4">
-  <p className="new-nav-button">weewew</p>
+  <div className="nav-item col-sm-3">
+  <p className="new-nav-button text-center">weewew</p>
   </div>
-  <div className="nav-item col-sm-4">
-   <p className="new-nav-button">weewew</p> 
+  <div className="nav-item col-sm-3">
+   <p className="new-nav-button text-center">weewew</p> 
   </div>
-  <div className="nav-item col-sm-4">
-  <p className="new-nav-button">weewew</p> 
+  <div className="nav-item col-sm-3">
+  <p className="new-nav-button text-center">weewew</p> 
   </div>
-  <div className="nav-item col-sm-4 ">
-  <p className="new-nav-button">weewew</p> 
+  <div className="nav-item col-sm-3">
+  <p className="new-nav-button text-center">weewew</p> 
   </div>
   
 </div></div>
