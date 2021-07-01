a541900c3d41ca133ea3c3c1574363fc7f3336a7
dylan <dylanabozarth@gmail.com>
2021-07-01 09:14:22 -0700

    new navbar size and font

--- src/newcomponents/newnav.js
+++ src/newcomponents/newnav.js
@@ -5,29 +5,27 @@ export const NewNav = () => {
   const [open1, setOpen] = useState(false);
 
   const [open2, setOpen2] = useState(false);
-  
 
-  
-const OpenNum1 = () => {
-  setOpen(!open1)
-  setOpen2(false)
-}
-const OpenNum2 = () => {
-  setOpen2(!open2)
-  setOpen(false)
-}
+  const OpenNum1 = () => {
+    setOpen(!open1);
+    setOpen2(false);
+  };
+  const OpenNum2 = () => {
+    setOpen2(!open2);
+    setOpen(false);
+  };
   return (
     <div className="container-fluid">
       <div className="  row">
-        <div className="nav-item col-sm-3" onClick={() => OpenNum1() }>
-          <p className="new-nav-button text-center nav-button-text">
+        <div className="nav-item col-sm-3" onClick={() => OpenNum1()}>
+          <p className="new-nav-button text-center nav-button-text puppyCardText">
             Infurmation
           </p>
           <div className={open1 === false ? `noShow` : `dropdown1 text-center`}>
             <NavLink to="/about" className="drop-down-text puppyCardText">
               <p className="drop-down-text puppyCardText">About us</p>
             </NavLink>
-            <NavLink to="/breedinfo"className="drop-down-text puppyCardText">
+            <NavLink to="/breedinfo" className="drop-down-text puppyCardText">
               <p className="drop-down-text puppyCardText">Doodle Facts</p>
             </NavLink>
             <NavLink to="/FAQ" className="drop-down-text puppyCardText">
@@ -41,10 +39,10 @@ const OpenNum2 = () => {
         <div
           className="nav-item col-sm-3"
           onClick={() => {
-           OpenNum2()
+            OpenNum2();
           }}
         >
-          <p className="new-nav-button text-center nav-button-text">Dogs</p>
+          <p className="new-nav-button text-center nav-button-text puppyCardText">Dogs</p>
           <div className={open2 === false ? `noShow` : `dropdown2 text-center`}>
             <NavLink to="/newpups" className="drop-down-text puppyCardText">
               <p className="drop-down-text puppyCardText">Current Litters</p>
@@ -52,7 +50,10 @@ const OpenNum2 = () => {
             <NavLink to="/featured" className="drop-down-text puppyCardText">
               <p className="drop-down-text puppyCardText">Featured Puppies</p>
             </NavLink>
-            <NavLink to="/rainbowbridge" className="drop-down-text puppyCardText">
+            <NavLink
+              to="/rainbowbridge"
+              className="drop-down-text puppyCardText"
+            >
               <p className="drop-down-text puppyCardText">Past Doodles</p>
             </NavLink>
             <NavLink to="/application" className="drop-down-text puppyCardText">
@@ -61,10 +62,22 @@ const OpenNum2 = () => {
           </div>
         </div>
         <div className="nav-item col-sm-3">
-          <NavLink to="/reviews" className=" puppyCardText"><p className="new-nav-button text-center nav-button-text">Reviews</p></NavLink>
+          <NavLink to="/reviews" className=" puppyCardText">
+            <p className="new-nav-button text-center nav-button-text">
+              Reviews
+            </p>
+          </NavLink>
         </div>
         <div className="nav-item col-sm-3">
-          <a href="https://www.pawtree.com/jymrdoodles/myrecommendation/Standard-Package?fbclid=IwAR1x9IKyAO0ojODicSbSXWO7305bY4A3jnFoJZvPQaAHLBcMmMnqhjZXhPY" className="puppyCardText">  <p className="new-nav-button text-center nav-button-text">Pawtree</p></a>
+          <a
+            href="https://www.pawtree.com/jymrdoodles/myrecommendation/Standard-Package?fbclid=IwAR1x9IKyAO0ojODicSbSXWO7305bY4A3jnFoJZvPQaAHLBcMmMnqhjZXhPY"
+            className="puppyCardText"
+          >
+            {" "}
+            <p className="new-nav-button text-center nav-button-text">
+              Pawtree
+            </p>
+          </a>
         </div>
       </div>
     </div>
